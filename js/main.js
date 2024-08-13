9eb3217fe6be0603ed9f8dcd3e0d95b3cf643b4c
Brian <31705402+BrianCh16@users.noreply.github.com>
2024-08-13 17:26:22 -0400

    added JS responsive year in footer copyright

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// grabs date object year datetime value and sets the attribute to year ID in html.
 const year = document.getElementById("year");
 const thisYear = new Date().getFullYear();
 year.setAttribute("datetime", thisYear);
